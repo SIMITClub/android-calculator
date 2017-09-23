d9a71ac48248f8e80c3fce0c3e8425e688072dd2
Smit <smitshah95@hotmail.com>
2017-09-23 15:54:10 +0800

    implemented additions

--- app/src/main/java/a2017/simitclub/com/calculator/MainActivity.java
+++ app/src/main/java/a2017/simitclub/com/calculator/MainActivity.java
@@ -7,17 +7,20 @@ package a2017.simitclub.com.calculator;
         import android.widget.GridView;
         import android.widget.AdapterView;
         import android.widget.ArrayAdapter;
+        import android.widget.Switch;
         import android.widget.TextView;
         import android.widget.Toast;
 
 public class MainActivity extends Activity {
     String msg = "Android : ";
     TextView calculationView;
+    TextView resultView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         calculationView = (TextView) findViewById(R.id.opertationView);
+        resultView =  (TextView) findViewById(R.id.result);
     }
 
     /** Called when the activity is about to become visible. */
@@ -56,12 +59,31 @@ public class MainActivity extends Activity {
     }
 
     /** Called to concat the calculations and calculate*/
-    public void BuildCalculation(View v)
+    public void AppendOperator(View v)
     {
         String currentText = ((TextView) v).getText().toString();
-        Toast.makeText(getApplicationContext(),
-                currentText, Toast.LENGTH_SHORT).show();
         calculationView.append(currentText);
+    }
+
+    public void OperateCalculation(View v)
+    {
+        String currentText = ((TextView) v).getText().toString();
+        calculationView.append(currentText);
+        Compute(v);
+    }
+
+    /** Compute the whole expression
+     * 3 and 2 operands
+     * + operator */
+    public void Compute(View v) {
+        double result = 0.0;
+        String[] numbers = calculationView.getText().toString().split("\\+");
+
+        for (int i = 0; i < numbers.length; i++) {
+            result += Integer.parseInt(numbers[i]);
+        }
+
+        resultView.setText(String.format("%s", result));
     }
 
     /** This method will clear the view and calculations*/
